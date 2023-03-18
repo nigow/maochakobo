698e707ae68d5e83b5cf694666d04caf590986a3
tairi <tairi.goto@mail.utoronto.ca>
2023-03-18 15:33:38 +0900

    Updates

---
+++ precache-manifest.32d1ee30d18ae415ae3d803e8763f490.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bacc3884b6fe7c5ebf6",
+    "url": "/maochakobo/static/css/main.1b91dd6f.chunk.css"
+  },
+  {
+    "revision": "6bacc3884b6fe7c5ebf6",
+    "url": "/maochakobo/static/js/main.6bacc388.chunk.js"
+  },
+  {
+    "revision": "a45f3fc8e6afda5c6ad7",
+    "url": "/maochakobo/static/js/1.a45f3fc8.chunk.js"
+  },
+  {
+    "revision": "c7a4e9f94b336f5f4cd9",
+    "url": "/maochakobo/static/js/runtime~main.c7a4e9f9.js"
+  },
+  {
+    "revision": "1e924f20694657c8bc83a961ad77ba0a",
+    "url": "/maochakobo/index.html"
+  }
+];
